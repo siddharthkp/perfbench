84a07bf5c209f226d984a14f11f11456274cac10
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-05-11 19:02:15 +0530

    downloading where

--- src/setup.js
+++ src/setup.js
@@ -5,7 +5,8 @@ process.env.LIGHTHOUSE_CHROMIUM_PATH = `${process.cwd()}/chrome-linux/chrome`
 
 const commands = [
   'sh -e /etc/init.d/xvfb start',
-  'time ./node_modules/lighthouse/lighthouse-core/scripts/download-chrome.sh'
+  'time ./node_modules/lighthouse/lighthouse-core/scripts/download-chrome.sh',
+  'ls'
 ]
 
 const setup = () => {
