950be3b8b57ae1d90935530c63960c573491fdd9
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-10 20:19:53 +0530

    add download script

--- src/setup.js
+++ src/setup.js
@@ -3,10 +3,7 @@ const syncExec = require('sync-exec')
 process.env.DISPLAY = ':99.0'
 process.env.LIGHTHOUSE_CHROMIUM_PATH = `${process.cwd()}/chrome-linux/chrome`
 
-const commands = [
-  'sh -e /etc/init.d/xvfb start',
-  './node_modules/lighthouse/lighthouse-core/scripts/download-chrome.sh'
-]
+const commands = ['sh -e /etc/init.d/xvfb start', 'sh ./src/download-chrome.sh']
 
 const setup = () => {
   console.log('Setting up Chrome')
