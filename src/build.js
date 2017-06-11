b5fa2f9d907d0db9b265ffb9c4107db4a88b1522
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-11 20:22:21 +0530

    Test with messages without emoji

--- src/build.js
+++ src/build.js
@@ -16,8 +16,8 @@ let fail = () => process.exit(1)
 /* If github token is given and we have commit sha */
 if (process.env.github_token && process.env.TRAVIS_PULL_REQUEST_SHA) {
   build.start()
-  pass = () => build.pass()
-  fail = () => build.fail()
+  pass = () => build.pass('Performance checks passed!')
+  fail = () => build.fail('Performance checks failed!')
 }
 
 module.exports = { pass, fail }
