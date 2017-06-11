610dad5abcb7ab247c19d7c86de9be2b50f8a2d0
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-11 17:57:06 +0530

    init build

--- src/build.js
+++ src/build.js
@@ -1,14 +1,14 @@
-// const Build = require('github-build')
+const Build = require('github-build')
 
-// const data = {
-//   repo: process.env.TRAVIS_REPO_SLUG,
-//   sha: process.env.TRAVIS_PULL_REQUEST_SHA,
-//   token: process.env.github_token,
-//   label: 'perfbench',
-//   description: 'Running performance tests'
-// }
+const data = {
+  repo: process.env.TRAVIS_REPO_SLUG,
+  sha: process.env.TRAVIS_PULL_REQUEST_SHA,
+  token: process.env.github_token,
+  label: 'perfbench',
+  description: 'Running performance tests'
+}
 
-// const build = new Build(data)
+const build = new Build(data)
 
 let pass = () => {} // noop
 let fail = () => process.exit(1)
