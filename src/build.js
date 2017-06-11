db5729184be1096da674bec73d15012be31a99a0
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-11 17:05:15 +0530

    always making typos

--- src/build.js
+++ src/build.js
@@ -1,7 +1,7 @@
 const Build = require('github-build')
 
 const data = {
-  repo: provess.env.TRAVIS_REPO_SLUG,
+  repo: process.env.TRAVIS_REPO_SLUG,
   sha: process.env.TRAVIS_PULL_REQUEST_SHA,
   token: process.env.github_token,
   label: 'perfbench',
