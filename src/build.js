3aa664455cb97848c6b3ccf417c05048400ba4bd
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-11 17:24:17 +0530

    add commit sha check

--- src/build.js
+++ src/build.js
@@ -10,19 +10,15 @@ const data = {
 console.log(data)
 
 const build = new Build(data)
-console.log(build)
 
 let pass = () => {} // noop
 let fail = () => process.exit(1)
 
-/* If github token is given */
-if (process.env.github_token) {
-  console.log('enhancing pass + fail')
+/* If github token is given and we have commit sha */
+if (process.env.github_token && process.env.TRAVIS_PULL_REQUEST_SHA) {
   build.start()
   pass = () => build.pass()
   fail = () => build.fail()
 }
-
-console.log(pass)
 
 module.exports = { pass, fail }
