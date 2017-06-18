654359c5236b65105fe632920370b4b8dc85e0f7
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-19 01:41:54 +0530

    add message on pass

--- src/build.js
+++ src/build.js
@@ -3,6 +3,7 @@ const startcase = require('lodash.startcase')
 const { thresholds } = require('./settings')
 const { units } = require('./properties')
 const { repo, sha, token } = require('./travis')
+const store = require('./api')
 
 const label = 'perfbench'
 const description = 'Running performance tests...'
@@ -16,9 +17,37 @@ let fail = () => process.exit(1)
 /* If github token is given and we have commit sha */
 if (token && sha) {
   build.start()
-  pass = values => build.pass('Performance checks passed!')
+
+  pass = values => {
+    if (store.enabled) {
+      const master = store.get()
+
+      let properties = []
+      let message
+
+      const keys = ['time-to-interactive', 'first-meaningful-paint']
+      let increased = false
+
+      for (let key of keys) {
+        if (values[key] > master[key]) {
+          increased = key
+          break
+        }
+      }
+
+      if (increased) {
+        const key = increased
+        message = `${startcase(key)} has increased by ${values[key] - threshold[key]} ${units(key)}`
+      } else {
+        const key = keys[0]
+        message = `${startcase(key)} has improved by ${threshold[key] - values[key]} ${units(key)}`
+      }
+    }
+    build.pass('Performance checks passed!')
+  }
+
   fail = values => {
-    const properties = []
+    let properties = []
     let message
 
     const keys = Object.keys(values)
