7260ba2c9fcbc52668eaa55c9d91760a26aa183f
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-19 01:14:59 +0530

    add a message in fail case

--- src/build.js
+++ src/build.js
@@ -1,5 +1,7 @@
 const Build = require('github-build')
-let { thresholds } = require('./settings')
+const startcase = require('lodash.startcase')
+const { thresholds } = require('./settings')
+const { units } = require('./properties')
 const { repo, sha, token } = require('./travis')
 
 const label = 'perfbench'
@@ -15,7 +17,27 @@ let fail = () => process.exit(1)
 if (token && sha) {
   build.start()
   pass = values => build.pass('Performance checks passed!')
-  fail = values => build.fail('Performance checks failed!')
+  fail = values => {
+    const properties = []
+    let message
+
+    const keys = Object.keys(values)
+    for (key of keys) {
+      if (values[key] > thresholds[key]) properties.push(key)
+    }
+
+    if (properties.length === 1) {
+      const key = properties[0]
+      message = `${startcase(key)} is above threshold (${thresholds[key]} ${units(key)})`
+    } else {
+      message = `${properties
+        .map(p => startcase(p))
+        .join(', ')
+        .replace(/,([^,]*)$/, ' and$1')} are above threshold`
+    }
+
+    build.fail(message)
+  }
 }
 
 module.exports = { pass, fail }
