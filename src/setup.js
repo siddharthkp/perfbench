32101544cb97e8d50d654cc6234c16c276f49eda
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-07-26 00:30:46 +0530

    more debug statements

--- src/setup.js
+++ src/setup.js
@@ -3,8 +3,11 @@ const syncExec = require('sync-exec')
 const path = require('path')
 
 const setup = () => {
+  console.log(1)
   process.env.DISPLAY = ':99.0'
+  console.log(2)
   const output = syncExec('sh -e /etc/init.d/xvfb start')
+  console.log(3)
   return seleniumAssistant.downloadLocalBrowser('chrome', 'stable')
 }
 
